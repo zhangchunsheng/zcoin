7cee1ec932209dede90a4acea7e369127d6ad2ca
zhangchunsheng <zhangchunsheng423@gmail.com>
2018-06-09 17:56:58 +0800

    1.新功能:初始化项目

--- node/src/main.js
+++ node/src/main.js
@@ -1,1 +1,34 @@
 const SHA256 = require("crypto-js/sha256");
+class Block {
+    constructor(index, timestamp, data, previousHash = '') {
+        this.index = index;
+        this.previousHash = previousHash;
+        this.timestamp = timestamp;
+        this.data = data;
+        this.hash = this.calculateHash();
+    }
+
+    calculateHash() {
+        return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data)).toString();
+    }
+}
+
+class Blockchain {
+    constructor() {
+        this.chain = [this.createGenesisBlock()];
+    }
+
+    createGenesisBlock() {
+        return new Block(0, "2018-01-01", "Genesis block", "0");
+    }
+
+    getLatestBlock() {
+        return this.chain[this.chain.length - 1];
+    }
+
+    addBlock(newBlock) {
+        newBlock.previousHash = this.getLatestBlock().hash;
+        newBlock.hash = newBlock.calculateHash();
+        this.chain.push(newBlock);
+    }
+}
