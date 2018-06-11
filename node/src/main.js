df8caa85f7edb3a28a22efb1d40ed4645987a161
zhangchunsheng <zhangchunsheng423@gmail.com>
2018-06-11 15:52:31 +0800

    1.新功能:初始化项目

--- node/src/main.js
+++ node/src/main.js
@@ -1,4 +1,5 @@
 const SHA256 = require("crypto-js/sha256");
+
 class Block {
     constructor(index, timestamp, data, previousHash = '') {
         this.index = index;
@@ -31,4 +32,29 @@ class Blockchain {
         newBlock.hash = newBlock.calculateHash();
         this.chain.push(newBlock);
     }
+
+    isChainValid() {
+        for(let i = 1 ; i < this.chain.length ; i++) {
+            const currentBlock = this.chain[i];
+            const previousBlock = this.chain[i - 1];
+
+            if(currentBlock.hash != currentBlock.calculateHash()) {
+                return false;
+            }
+            if(currentBlock.previousHash != previousBlock.hash) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
+
+let zCoin = new Blockchain();
+zCoin.addBlock(new Block(1, "2018-06-11", { amount: 4 }));
+zCoin.addBlock(new Block(2, "2018-06-11", { amount: 8 }));
+
+console.log('Blockchain valid? ' + zCoin.isChainValid());
+
+zCoin.chain[1].data = { amount: 100 };
+
+console.log("Blockchain valid? " + zCoin.isChainValid());
